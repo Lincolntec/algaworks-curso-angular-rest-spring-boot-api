82a4f88fd36eb0bbdda75eed27a5a385e3d86760
LincolnTec <lincoln@lincolntec.com.br>
2019-12-05 04:02:58 -0300

    implementado delete do lancamento

--- src/main/java/com/example/algamoneyapi/resource/LancamentoResource.java
+++ src/main/java/com/example/algamoneyapi/resource/LancamentoResource.java
@@ -13,6 +13,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,51 +33,49 @@ import com.example.algamoneyapi.service.LancamentoService;
 @RestController
 @RequestMapping("/lancamentos")
 public class LancamentoResource {
-	
+
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
-	
+
 	@Autowired
 	private ApplicationEventPublisher publisher;
-	
+
 	@Autowired
 	private LancamentoService lancamentoService;
-	
+
 	@Autowired
 	private MessageSource messageSource;
-	
+
 	@GetMapping
-	public List<Lancamento> pesquisar(LancamentoFilter lancamentoFilter){
-		
+	public List<Lancamento> pesquisar(LancamentoFilter lancamentoFilter) {
+
 		return lancamentoRepository.filtrar(lancamentoFilter);
 	}
-	
+
 	@PostMapping
 	public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
-		
+
 		Lancamento lancamentoSalvo = lancamentoService.salvar(lancamento);
-		
-		publisher.publishEvent( new RecursoCriadoEvent(this, response, lancamento.getCodigo()));
-		
+
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamento.getCodigo()));
+
 		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
-		
+
 	}
-	
+
 	@GetMapping("/{codigo}")
 	public ResponseEntity<Lancamento> buscaLancamentoPorCodigo(@PathVariable Long codigo) {
-		
-		Optional<Lancamento> lancamentoSalvo =  lancamentoRepository.findById(codigo);
-		
+
+		Optional<Lancamento> lancamentoSalvo = lancamentoRepository.findById(codigo);
+
 		return lancamentoSalvo.isPresent() ? ResponseEntity.ok(lancamentoSalvo.get())
 				: ResponseEntity.notFound().build();
-			
-		
-		
+
 	}
-	
-	@ExceptionHandler({ PessoaInexistenteOuInativaException.class } )
-	public ResponseEntity<Object> hadelerPessoaInexistenteOuInativaException(PessoaInexistenteOuInativaException ex){
-		
+
+	@ExceptionHandler({ PessoaInexistenteOuInativaException.class })
+	public ResponseEntity<Object> hadelerPessoaInexistenteOuInativaException(PessoaInexistenteOuInativaException ex) {
+
 		String mensagemUsuario = messageSource.getMessage("pessoa.inexistente-ou-inativa", null,
 				LocaleContextHolder.getLocale());
 		String messagemDesenvolvedor = ex.toString();
@@ -84,7 +83,13 @@ public class LancamentoResource {
 		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, messagemDesenvolvedor));
 
 		return ResponseEntity.badRequest().body(erros);
-		
+
 	}
 
+	@DeleteMapping("/{codigo}")
+	public void remover(@PathVariable Long codigo) {
+
+		lancamentoRepository.deleteById(codigo);
+
+	}
 }
